0d698c9c77aafb3f3fe37ba07f8793dd778a38af
Sam A Miller <smills701@gmail.com>
2023-08-18 16:27:31 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ setup(
     version='0.1.0',
     url='https://github.com/SamuelAMiller1/py-scProportionTest',
     author='Sam Miller',
-    description='Python package equivalent to R package scProportionTest',
     packages=find_packages(),    
-    install_requires=['numpy', 'pandas', 'anndata', 'scanpy'],
+    install_requires=['numpy', 'pandas', 'statsmodels', 'matplotlib', 'tqdm'],
+    description='Python package to evaluate differences in cell type proportions',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown'
 )
