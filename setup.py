e12913c7302e66b2abe997c3ee0446fb3d68be8e
Sam A Miller <smills701@gmail.com>
2023-08-18 18:01:42 -0400

    update release version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='py-scProportionTest',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/SamuelAMiller1/py-scProportionTest',
     author='Sam Miller',
     packages=find_packages(),    
