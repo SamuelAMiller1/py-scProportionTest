39efa100d035f105a9f76fe2ea6b9a64e34b3c3a
Sam A Miller <smills701@gmail.com>
2023-08-25 15:26:09 -0700

    Update __init__.py
    add imports for multiple permutation

--- scProportionTest/__init__.py
+++ scProportionTest/__init__.py
@@ -4,3 +4,5 @@ __version__ = '0.1.1'
 #package imports
 from .permutation_test import permutation_test
 from .plot import point_range_plot
+from .multiple_permutation_test import multiple_permutation_test
+from .multiple_permutation_dot_plot import multiple_permutation_dot_plot
