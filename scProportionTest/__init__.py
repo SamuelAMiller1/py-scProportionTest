edd13bcba966b8cbc1e5e49fcec460a9c6aee1dc
Sam A Miller <smills701@gmail.com>
2023-08-27 16:35:21 -0700

    Update __init__.py

--- scProportionTest/__init__.py
+++ scProportionTest/__init__.py
@@ -2,7 +2,8 @@
 __version__ = '0.1.1'
 
 #package imports
-from .permutation_test import permutation_test
-from .plot import point_range_plot
-from .multiple_permutation_test import multiple_permutation_test
-from .multiple_permutation_dot_plot import multiple_permutation_dot_plot
+from ._permutation_test import permutation_test
+from ._point_range_plot import point_range_plot
+from ._multiple_permutation_test import multiple_permutation_test
+from ._multiple_permutation_dot_plot import multiple_permutation_dot_plot
+from ._multiple_point_range_plot import multiple_permutation_dot_plot
