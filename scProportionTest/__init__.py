632bef3f6d47de4996714f9f647aaa87b068ccff
Sam A Miller <smills701@gmail.com>
2023-09-09 11:23:02 -0700

    Update __init__.py
    add cluster_map import

--- scProportionTest/__init__.py
+++ scProportionTest/__init__.py
@@ -7,3 +7,4 @@ from ._point_range_plot import point_range_plot
 from ._multiple_permutation_test import multiple_permutation_test
 from ._multiple_permutation_dot_plot import multiple_permutation_dot_plot
 from ._multiple_point_range_plot import multiple_point_range_plot
+from ._cluster_map import cluster_map
