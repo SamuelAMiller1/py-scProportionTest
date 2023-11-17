14ef9ae0a9d7733733d722fe4f2d3b0a00be2b26
Sam A Miller <smills701@gmail.com>
2023-11-17 17:57:58 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,12 @@ from scProportionTest import __version__
 
 with open('README.rst', 'r') as file:
     long_description = file.read()
-
 setup(
     name='py-scProportionTest',
     version=__version__,
     url='https://github.com/SamuelAMiller1/py-scProportionTest',
     author='Sam Miller',
-    packages=find_packages(),    
+    packages=find_packages(),
     install_requires=[
         'numpy',
         'pandas',
